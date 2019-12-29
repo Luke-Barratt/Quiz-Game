bd477d5f0c9c844502164572f7e663791e535791
Luke <luke_barratt130@hotmail.com>
2019-12-29 14:18:12 +0000

    Server class update.

--- src/com/quiz/lukebarratt/Server.java
+++ src/com/quiz/lukebarratt/Server.java
@@ -1,15 +1,57 @@
 package com.quiz.lukebarratt;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+import java.util.Map;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) throws IOException {
+        XMLParser xmlparser = new XMLParser("questionaire.xml");
 
-        //server is listening on port 5036
+        Map<Integer, Question> questions = xmlparser.getXMLParser();
+        int players = xmlparser.getNumOfPlayers();
+
+        System.out.println("Number of players: " + players);
+
+        ClientHandler[] playerThreads = new ClientHandler[players];
+
+        try{
+            ServerSocket serverSocket = new ServerSocket(5036);
+            int playerCounter = 0;
+            while (true) {
+                Socket server = serverSocket.accept();
+                System.out.println("Player " + playerCounter + " has joined the server");
+
+                playerThreads[playerCounter] = new ClientHandler(server, playerCounter, questions);
+                playerCounter++;
+                if(playerCounter == players){
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for(ClientHandler playerThread : playerThreads){
+            playerThread.start();
+        }
+
+        for(ClientHandler playerThread : playerThreads){
+            playerThread.join();
+        }
+
+        int max = -1;
+        String player = null;
+
+        for(ClientHandler playerThread : playerThreads) {
+            playerThread.sendFinalMessage(player + " has won the quiz!")
+        }
+
+        System.out.println("Exiting game...");
+
+    }
+/*        //server is listening on port 5036
         ServerSocket serverSocket = new ServerSocket(5036);
         //running infinite lop to wait for client server request
         while (true) {
@@ -36,5 +78,5 @@ public class Server {
                 e.printStackTrace();
             }
         }
-    }
+    }*/
 }
